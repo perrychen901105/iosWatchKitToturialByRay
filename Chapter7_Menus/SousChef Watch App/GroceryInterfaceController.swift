eb2d66b7b42e45ed53d1613626edab108a9eab00
perrychen901105 <chenzhipeng901105@gmail.com>
2015-04-27 20:56:08 +0800

    finish chapter 7 code

--- Chapter7_Menus/SousChef Watch App/GroceryInterfaceController.swift
+++ Chapter7_Menus/SousChef Watch App/GroceryInterfaceController.swift
@@ -1,24 +1,4 @@
-/*
-* Copyright (c) 2015 Razeware LLC
-*
-* Permission is hereby granted, free of charge, to any person obtaining a copy
-* of this software and associated documentation files (the "Software"), to deal
-* in the Software without restriction, including without limitation the rights
-* to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
-* copies of the Software, and to permit persons to whom the Software is
-* furnished to do so, subject to the following conditions:
-*
-* The above copyright notice and this permission notice shall be included in
-* all copies or substantial portions of the Software.
-*
-* THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
-* IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
-* FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
-* AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
-* LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
-* OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN
-* THE SOFTWARE.
-*/
+
 
 import Foundation
 import WatchKit
@@ -28,12 +8,54 @@ class GroceryInterfaceController: WKInterfaceController {
 
   @IBOutlet weak var table: WKInterfaceTable!
 
-  let groceryList = GroceryList(useSample: true)
+  let groceryList = GroceryList()
 
   lazy var flatList: [FlatGroceryItem] = {
     return self.groceryList.flattenedGroceries()
     }()
 
+    
+    @IBAction func onClearAll() {
+        // 1
+      let indices = NSIndexSet(indexesInRange: NSRange(location: 0, length: table.numberOfRows))
+      table.removeRowsAtIndexes(indices)
+      
+      // 2
+      groceryList.removeAllItems()
+      groceryList.sync()
+      
+      // 3
+      for (index, listItem) in enumerate(flatList) {
+        if let item = listItem.item as? Ingredient {
+          item.purchased = false
+        }
+      }
+      
+      // 4
+      flatList = self.groceryList.flattenedGroceries()
+    }
+    
+    @IBAction func onRemovePurchased() {
+        // 1
+      var indexSet = NSMutableIndexSet()
+      
+      // 2
+      for (index, listItem) in enumerate(flatList) {
+        if let item = flatList[index].item as? Ingredient {
+          if item.purchased {
+            indexSet.addIndex(index)
+            groceryList.removeItem(item)
+          }
+        }
+      }
+      groceryList.sync()
+      
+      // 3
+      table.removeRowsAtIndexes(indexSet)
+      flatList = self.groceryList.flattenedGroceries()
+    }
+    
+    
   var cellTextAttributes: [NSObject: AnyObject] {
     return [
       NSFontAttributeName: UIFont.systemFontOfSize(16),
