817bf5d0a54151e2ae245bc387f455d8dbf3e006
Perry <chenzhipeng901105@gmail.com>
2015-04-29 18:00:08 +0800

    add chapter 11 code

---
+++ Chapter11_Notification/SousChef Watch App/HandoffConstants.swift
@@ -0,0 +1,15 @@
+//
+//  HandoffConstants.swift
+//  SousChef
+//
+//  Created by chenzhipeng on 4/29/15.
+//  Copyright (c) 2015 Ray Wenderlich. All rights reserved.
+//
+
+import Foundation
+
+
+let kHandoffVersionKey = "version"
+let kHandoffVersionNumber = "1.0"
+let kGlanceHandoffActivityName = "com.rw.souschef.glance"
+let kGlanceHandoffNextItemKey = "nextItem"
