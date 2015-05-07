c5b4649a5923a2601416012d1ae2c984efe5f2d9
Perry <chenzhipeng901105@gmail.com>
2015-05-07 21:03:28 +0800

    add chapter 12 code

--- Chapter12_Animation/ArcGenerator/ArcGenerator/ViewController.swift
+++ Chapter12_Animation/ArcGenerator/ArcGenerator/ViewController.swift
@@ -13,15 +13,46 @@ class ViewController: UIViewController {
   override func viewDidLoad() {
     super.viewDidLoad()
     // Do any additional setup after loading the view, typically from a nib.
-    
+
+    // 1
     let generator = ArcGenerator()
-    let animation = ArcAnimation.spinningArcAnimation()
-    animation.name = "spinning"
+    let width = 10
+    let radius = 60
+    let gap = 2
+    
+    // 2
+    var animation = ArcAnimation.progressArcAnimation()
+    animation.totalFrames = 100
+    animation.name = "progressRed"
+    
+    // 3
+    animation.initialArc = Arc(radius: radius,
+      lineWidth: width, padding: 0,
+      startAngle: 3.0 * M_PI / 2.0, endAngle: 3.0 * M_PI / 2.0,
+      clockwise: true, color: UIColor.magentaColor())
+    animation.initialArc.emptyArcColor = UIColor.magentaColor().colorWithAlphaComponent(0.3)
+    animation.initialArc.lineWidth = width
+    
+    // 4
     generator.generateArcAnimationFrames(animation)
     
-    let animation2 = ArcAnimation.progressArcAnimation()
-    animation2.name = "progress"
-    generator.generateArcAnimationFrames(animation2)
+    
+    
+//    let generator = ArcGenerator()
+//    let animation = ArcAnimation.progressArcAnimation()
+//    animation.name = "progress"
+//    animation.initialArc.color = UIColor.magentaColor()
+//    animation.initialArc.emptyArcColor = UIColor.magentaColor().colorWithAlphaComponent(0.3)
+//    generator.generateArcAnimationFrames(animation)
+    
+//    let generator = ArcGenerator()
+//    let animation = ArcAnimation.spinningArcAnimation()
+//    animation.name = "spinning"
+//    generator.generateArcAnimationFrames(animation)
+//    
+//    let animation2 = ArcAnimation.progressArcAnimation()
+//    animation2.name = "progress"
+//    generator.generateArcAnimationFrames(animation2)
   }
   
 }
